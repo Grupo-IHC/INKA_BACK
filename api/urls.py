f20a9a395ab30dfc9052c2dadaf6b12501a433ea
Esaul Calle <ecalle@aios.pe>
2024-04-02 01:58:14 -0500

    fix url register

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('client', views.ClientGetPost.as_view(), name='client_register'),
+    
 ]
--- security/urls.py
+++ security/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('login', views.UserLoginView.as_view(), name='login'),
     path('token/verify/', views.VerifyView.as_view(), name='token_verify'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('register', views.UserRegisterView.as_view(), name='client_register'),
     # path('envs', views.EnvsView.as_view(), name='envs'),
 ] 
