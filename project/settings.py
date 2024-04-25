f999680e938189b5dec8f599435426acad66ee8a
Esaul Calle <ecalle@aios.pe>
2024-04-25 00:56:16 -0500

    test4

--- project/settings.py
+++ project/settings.py
@@ -68,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.backends.db.SessionStore',
+    'django.contrib.sessions.backends.db'
 ]
 
 ROOT_URLCONF = 'project.urls'
